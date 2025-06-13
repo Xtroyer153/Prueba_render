8fd42b247366f98ce8dea821a9dd74f5cce60221
BrianQ <bquicio@gmail.com>
2025-06-13 17:27:46 -0500

    Primera subida del proyecto Flask + modelo CNN

--- app.py
+++ app.py
@@ -5,20 +5,21 @@ from tensorflow.keras.preprocessing import image
 import os
 from werkzeug.utils import secure_filename
 import shutil
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-# Cargar modelo entrenado
-model = tf.keras.models.load_model('brain_tumor_cnn.h5')
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
-# Diccionario de clases
-class_names = ['glioma', 'meningioma', 'notumor', 'pituitary']
-
-# Carpeta de subida
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+model = tf.keras.models.load_model('brain_tumor_cnn.h5', compile=False)
+
+class_names = ['glioma', 'meningioma', 'notumor', 'pituitary']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -33,25 +34,21 @@ def index():
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
 
-            # Copiar imagen a carpeta static/uploads para previsualizar
             static_folder = os.path.join('static', 'uploads')
             os.makedirs(static_folder, exist_ok=True)
             static_path = os.path.join(static_folder, filename)
             shutil.copy(filepath, static_path)
 
-            # Preprocesar imagen
             img = image.load_img(filepath, target_size=(128, 128))
             img_array = image.img_to_array(img)
             img_array = np.expand_dims(img_array, axis=0)
             img_array = img_array / 255.0
 
-            # Predicción
             prediction = model.predict(img_array)[0]
             predicted_class = class_names[np.argmax(prediction)]
             prediction_result = f"Predicción: {predicted_class.upper()}"
             probabilities = {class_names[i]: float(f"{prob:.4f}") for i, prob in enumerate(prediction)}
 
-            # Graficar probabilidades
             plt.figure(figsize=(6, 4))
             plt.bar(probabilities.keys(), probabilities.values(), color='skyblue')
             plt.title('Probabilidades por clase')
@@ -64,6 +61,4 @@ def index():
     return render_template('index.html', prediction=prediction_result, image_name=filename, probs=probabilities)
 
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
+    app.run(debug=False)
